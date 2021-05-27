6996f77a0721290b5db107820759693c83d845ae
avi.mishra <mishra.avi18@gmail.com>
2021-05-28 02:00:49 +0530

    init commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+new WinBox({
+  title: "guest@ubuntu",
+  // top: 50,
+  // right: 50,
+  // bottom: 50,
+  // left: 50,
+  x: "center",
+  y: "center",
+});
+
+/*
+const buttonOpenTerminal = document.querySelector("#openTerminal");
+buttonOpenTerminal.addEventListener("click", () => {
+  new WinBox("Basic Window");
+});
+*/
